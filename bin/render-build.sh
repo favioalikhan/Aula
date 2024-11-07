bda24c51a8bc3bf805a8ee1490a0e87579b9aae2
Favio <favio.alikhan30@gmail.com>
2024-11-07 08:05:42 -0500

    t

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,12 +5,12 @@ set -o errexit
 # Instalar las dependencias
 pip install --upgrade pip
 pip install -r requirements.txt
+pip install --upgrade wagtail
 
 # Si usas Poetry, descomenta la siguiente línea y comenta las líneas de pip
 # poetry install
-
+python manage.py collectstatic --noinput
 # Aplicar migraciones de la base de datos
 python manage.py migrate
 
 # Recopilar archivos estáticos
-python manage.py collectstatic --noinput
