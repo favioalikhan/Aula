7e74892f091170c5741bd304a456a6175e37af12
Favio <favio.alikhan30@gmail.com>
2024-11-07 08:24:07 -0500

    superuser

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -13,4 +13,22 @@ python manage.py collectstatic --noinput
 # Aplicar migraciones de la base de datos
 python manage.py migrate
 
-# Recopilar archivos estáticos
+# Crear el superusuario si no existe
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+import os
+
+User = get_user_model()
+email = os.getenv('SUPERUSER_EMAIL')
+username = os.getenv('SUPERUSER_USERNAME')
+password = os.getenv('SUPERUSER_PASSWORD')
+
+if email and username and password:
+    if not User.objects.filter(email=email).exists():
+        User.objects.create_superuser(email=email, username=username, password=password)
+        print("Superusuario creado exitosamente.")
+    else:
+        print("El superusuario ya existe.")
+else:
+    print("No se proporcionaron las credenciales del superusuario.")
+EOF
